ea6834e125339381ab34256e8c6acc1bdc97fec4
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-10-28 21:06:12 +0600

    commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,14 +5,37 @@ import {getAuth} from "firebase/auth"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
+
+
+
+
+
+
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCBt6y65VqgOWLMKibFeSNkp9uPnm1jySs",
+  authDomain: "genius-car-servics.firebaseapp.com",
+  projectId: "genius-car-servics",
+  storageBucket: "genius-car-servics.appspot.com",
+  messagingSenderId: "60613292370",
+  appId: "1:60613292370:web:95e2af6dc969e9f40aca8b"
 };
+
+
+
+
+
+
+// const firebaseConfig = {
+//   apiKey:process.env.REACT_APP_apiKey,
+//   authDomain:process.env.REACT_APP_authDomain,
+//   projectId:process.env.REACT_APP_projectId,
+//   storageBucket:process.env.REACT_APP_storageBucket,
+//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
+//   appId:process.env.REACT_APP_appId,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
