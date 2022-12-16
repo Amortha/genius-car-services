285d273dd37233df7c546c996b0ffde4a94568d0
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-12-16 10:06:35 +0600

    commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -14,29 +14,22 @@ import {getAuth} from "firebase/auth"
 
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCBt6y65VqgOWLMKibFeSNkp9uPnm1jySs",
-//   authDomain: "genius-car-servics.firebaseapp.com",
-//   projectId: "genius-car-servics",
-//   storageBucket: "genius-car-servics.appspot.com",
-//   messagingSenderId: "60613292370",
-//   appId: "1:60613292370:web:95e2af6dc969e9f40aca8b"
-// };
 const firebaseConfig = {
-
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  // apiKey:process.env.REACT_APP_apiKey,
-  // authDomain:process.env.REACT_APP_authDomain,
-  // projectId:process.env.REACT_APP_projectId,
-  // storageBucket:process.env.REACT_APP_storageBucket,
-  // messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  // appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCBt6y65VqgOWLMKibFeSNkp9uPnm1jySs",
+  authDomain: "genius-car-servics.firebaseapp.com",
+  projectId: "genius-car-servics",
+  storageBucket: "genius-car-servics.appspot.com",
+  messagingSenderId: "60613292370",
+  appId: "1:60613292370:web:95e2af6dc969e9f40aca8b"
 };
+// const firebaseConfig = {
+//   apiKey:process.env.REACT_APP_apiKey,
+//   authDomain:process.env.REACT_APP_authDomain,
+//   projectId:process.env.REACT_APP_projectId,
+//   storageBucket:process.env.REACT_APP_storageBucket,
+//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
+//   appId:process.env.REACT_APP_appId,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
